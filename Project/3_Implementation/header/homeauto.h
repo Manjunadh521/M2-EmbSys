5850f27befc9f2ade7dc49d651fd54cb6400e00f
manjunadh521 <manjunadh521@gmail.com>
2022-03-03 17:56:41 +0530

    commit

--- Project/3_Implementation/header/homeauto.h
+++ Project/3_Implementation/header/homeauto.h
@@ -31,5 +31,5 @@ void LCD_Clear();
 char keyfind();
 int ADC_Read(char channel);
 void ADC_Init();
-long map(long x, long in_min, long in_max, long out_min, long out_max) ;
+int map(int x, int in_min, int in_max, int out_min, int out_max);
 #endif
--- Project/3_Implementation/src/homeautofun.c
+++ Project/3_Implementation/src/homeautofun.c
@@ -178,6 +178,6 @@ int ADC_Read(char channel)
 	return(Ain);			        /* Return digital value*/
 }
 
- long map(long x, long in_min, long in_max, long out_min, long out_max) {
+ int map(int x, int in_min, int in_max, int out_min, int out_max) {
    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
  }
