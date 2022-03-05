a701af10b8a9deceb3eda4e131e6f50acd6a8f6d
manjunadh521 <manjunadh521@gmail.com>
2022-03-05 19:18:46 +0530

    commit

--- Project/3_Implementation/Unity_Test/test_project.c
+++ Project/3_Implementation/Unity_Test/test_project.c
@@ -3,22 +3,18 @@
 
 void setUp()
 {
-   
 }
 void tearDown()
 {
-} 
-
-void test_map(void) 
-{ 
-  TEST_ASSERT_EQUAL_INT(525,map(20, 492, 478, 20, 35));
-  TEST_ASSERT_EQUAL_INT(520,map(25, 492, 478, 20, 35));
-  TEST_ASSERT_EQUAL_INT(515,map(30, 492, 478, 20, 35));
-  TEST_ASSERT_EQUAL_INT(509,map(35, 492, 478, 20, 35));
 }
 
-
-
+void test_map(void)
+{
+  TEST_ASSERT_EQUAL_INT(525, map(20, 492, 478, 20, 35));
+  TEST_ASSERT_EQUAL_INT(520, map(25, 492, 478, 20, 35));
+  TEST_ASSERT_EQUAL_INT(515, map(30, 492, 478, 20, 35));
+  TEST_ASSERT_EQUAL_INT(509, map(35, 492, 478, 20, 35));
+}
 
 int main()
 {
